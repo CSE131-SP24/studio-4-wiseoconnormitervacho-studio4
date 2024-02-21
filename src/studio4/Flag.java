50eb797a1b6b8f71fe7f2ba3cf93dec273620714
camiwise <camiwise@camis-mbp.dhcp.wustl.edu>
2024-02-21 15:39:06 -0600

    studio 4 done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(46,250,5);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.2);
+		StdDraw.setPenColor(246,250,5);
+		StdDraw.filledRectangle(0.5, 0.5, 0.2, 0.15);
+		StdDraw.setPenColor(5,250,209);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+		StdDraw.setPenColor(5,250,209);
+		StdDraw.setPenRadius(0.05);
+		StdDraw.rectangle(0.5, 0.5, 0.3, 0.2);
+		StdDraw.setPenRadius(0.002);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,55 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		StdDraw.setPenColor(red, green, blue);
+		if(shape.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			if(filled==true) {
+				StdDraw.filledRectangle(x,y,halfWidth,halfHeight);
+			}
+			if(filled==false) {
+				StdDraw.rectangle(x,y,halfWidth,halfHeight);
+			}
+
+		}
+
+		if (shape.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			//radius
+			double halfWidth = in.nextDouble();
+			if(filled==true) {
+				StdDraw.filledCircle(x,y,halfWidth);
+			}
+			if(filled==false) {
+				StdDraw.circle(x,y,halfWidth);
+			}
+		}
+		if(shape.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			double[]x=new x 
+			if(filled==true) {
+				StdDraw.filledPolygon(x1,y1,x2,y2,x3,y3);
+			
+			}
+			
+		}
+
+
+		}
 	}
-}
